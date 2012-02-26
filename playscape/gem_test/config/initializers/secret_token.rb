2c0b1295c103db15c10deef11e74af459fddff6b
Brandon Satrom <bsatrom+gh@gmail.com>
2012-02-26 13:22:11 -0600

    completed initial generator support

---
+++ playscape/gem_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemTest::Application.config.secret_token = '67c94943f6cdb281c307a545d29e67188f9554f9c285a35f9aa61db08a7a21345cc604768936ba62c3a6c99a7e729cb45a493faca4e2fa77d19260fddbe5b74b'
